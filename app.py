e629b93239b7af215707632b53eb06660115a641
lbv_o <gromlbv@gmail.com>
2025-08-13 13:22:59 +0300

    fixed utils.js

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/utils/<path:path>')
+def utils(path):
+    return send_from_directory('static/utils', path)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5200, debug=True)
